bfb44d46c0c24feb44e42bc66b75039ddb4169cc
javimdr <javimdr93@gmail.com>
2024-12-08 11:32:40 +0100

    fix: FOLDER_ROOT path in experiments constants

--- src/octa_mosaic/experiments/constants.py
+++ src/octa_mosaic/experiments/constants.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # Folder constants, useful for jupyter notebooks
-PROJECT_ROOT = Path(__file__).parents[4]
+PROJECT_ROOT = Path(__file__).parents[3]
 
 ARTIFACTS_PATH = PROJECT_ROOT / "artifacts"
 DATA_PATH = PROJECT_ROOT / "data"
