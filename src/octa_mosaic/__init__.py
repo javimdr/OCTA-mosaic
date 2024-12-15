4f5adf22fa25cc733f8495246cfc9907e5214474
javimdr <javimdr93@gmail.com>
2024-12-15 23:18:50 +0100

    refactor: add MosaicProcess to init

--- src/octa_mosaic/__init__.py
+++ src/octa_mosaic/__init__.py
@@ -1,10 +1,12 @@
 from octa_mosaic.mosaic import mosaic_metrics, mosaic_utils
 from octa_mosaic.mosaic.builders import TemplateMatchingBuilder
 from octa_mosaic.mosaic.mosaic import Mosaic
+from octa_mosaic.mosaic.mosaic_processor import MosaicProcessor
 
 __all__ = [
     Mosaic,
     mosaic_metrics,
     mosaic_utils,
     TemplateMatchingBuilder,
+    MosaicProcessor,
 ]
