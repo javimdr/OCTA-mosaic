fb00bd61a49d24b2d575620e85c1d58b89962296
javimdr <javimdr93@gmail.com>
2024-12-17 13:57:01 +0100

    docs: add docstrings to Dataset class

--- src/octa_mosaic/experiments/data/dataset.py
+++ src/octa_mosaic/experiments/data/dataset.py
@@ -17,6 +17,17 @@ class Dataset:
     """
 
     def __init__(self, dataset_path: str = None, dataset_url: str = None):
+        """
+        Initializes the Dataset class. Downloads and extracts the dataset if it's not
+        already available locally.
+
+        Args:
+            dataset_path (str, optional): Path to the dataset. Defaults to DATASET_PATH.
+            dataset_url (str, optional): URL to download the dataset. Defaults to DATASET_URL.
+
+        Raises:
+            ValueError: If the dataset path does not exist and no URL is provided.
+        """
         self._dataset_path = dataset_path or DATASET_PATH
         dataset_url = dataset_url or DATASET_URL
 
@@ -36,6 +47,16 @@ class Dataset:
         self._cases = [DatasetCase(path) for path in sorted(self._cases_path)]
 
     def _download_and_extract(self, url: str, extract_to: str) -> None:
+        """
+        Downloads a ZIP file from the given URL and extracts it to the specified location.
+
+        Args:
+            url (str): URL to download the ZIP file from.
+            extract_to (str): Directory to extract the contents of the ZIP file.
+
+        Raises:
+            RuntimeError: If the download fails due to an HTTP error.
+        """
         response = requests.get(url, stream=True)
         with tempfile.NamedTemporaryFile(suffix=".zip") as tmp_file:
             if response.status_code == 200:
@@ -51,12 +72,33 @@ class Dataset:
                 zip_ref.extractall(extract_to)
 
     def get_cases(self) -> List[DatasetCase]:
+        """
+        Retrieves all cases in the dataset.
+
+        Returns:
+            List[DatasetCase]: A list of `DatasetCase` objects.
+        """
         return list(self._cases)
 
     def get_cases_paths(self) -> List[str]:
+        """
+        Retrieves the paths to all cases in the dataset.
+
+        Returns:
+            List[str]: A list of paths to cases.
+        """
         return list(self._cases_path)
 
     def get_case(self, case_id: str) -> DatasetCase:
+        """
+        Retrieves a specific case by its ID.
+
+        Args:
+            case_id (str): The ID of the case to retrieve.
+
+        Returns:
+            DatasetCase: The case corresponding to the provided ID.
+        """
         for case in self.get_cases():
             if case.get_ID() == case_id:
                 return case
@@ -64,3 +106,10 @@ class Dataset:
 
     def __str__(self) -> str:
         return f"Dataset({len(self.get_cases())} test cases)"
+
+
+dataset = Dataset()
+print(dataset._cases_path)
+print([case.get_ID() for case in dataset.get_cases()])
+print(len(dataset.get_cases()[0].get_images()))
+print(dataset.get_cases()[0].get_images()[0].shape)
